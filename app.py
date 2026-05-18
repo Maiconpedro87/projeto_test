b71e0e288c019c9c70be4571511f18f68f33451c
Maicon <pedro_poa87@hotmail.com>
2026-05-17 23:26:52 -0300

    Update Streamlit app to read CSV from URL

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Meu primeiro app Streamlit")
-st.write("Funcionou!")
+st.title("Leitura do CSV no Streamlit")
+
+# URL do arquivo CSV no GitHub (RAW)
+url = "https://raw.githubusercontent.com/Maiconpedro87/projeto_test/main/Untitled0.csv"
+
+# Lendo o CSV
+df = pd.read_csv(url)
+
+st.subheader("Prévia dos dados")
+st.dataframe(df)
+
